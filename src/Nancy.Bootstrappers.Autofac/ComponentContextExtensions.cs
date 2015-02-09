a8b39bb3339367dc7977a6cfa0f992d704e8d24b
Kristian Hellang <kristian@hellang.com>
2015-02-09 18:14:10 +0100

    Added XML docs to new extension method

--- src/Nancy.Bootstrappers.Autofac/ComponentContextExtensions.cs
+++ src/Nancy.Bootstrappers.Autofac/ComponentContextExtensions.cs
@@ -5,6 +5,14 @@ namespace Nancy.Bootstrappers.Autofac
 {
     public static class ComponentContextExtensions
     {
+        /// <summary>
+        /// Updates the specified component context using the specified <paramref name="builderAction"/>.
+        /// </summary>
+        /// <typeparam name="T">The component context type.</typeparam>
+        /// <param name="context">The component context to update.</param>
+        /// <param name="builderAction">The builder action.</param>
+        /// <returns>The updated component context.</returns>
+        /// <exception cref="System.ArgumentNullException">If <paramref name="context"/> or <paramref name="builderAction"/> is <c>null</c>.</exception>
         public static T Update<T>(this T context, Action<ContainerBuilder> builderAction) where T : IComponentContext
         {
             if (context == null)
